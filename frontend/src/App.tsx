2935b3fdf18957ae89f0ecc91166800589f9355c
paulogmedeiros <paulogsantosm@gmail.com>
2026-08-25 12:44:25 -0300

    feat(config lib frontend)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Routes } from 'react-router-dom';
+
+export function App() {
+  return (
+    <Routes>
+    </Routes>
+  );
+}
+
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss(),
+    ],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+    },
+  };
+});
